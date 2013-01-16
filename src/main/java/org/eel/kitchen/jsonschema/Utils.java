4816c35a5407eb8b8c6a62d8bc61c0128b84b1fe
Francis Galiegue <fgaliegue@gmail.com>
2013-01-16 19:09:22 +0100

    Add a hashcode function

--- src/main/java/org/eel/kitchen/jsonschema/Utils.java
+++ src/main/java/org/eel/kitchen/jsonschema/Utils.java
@@ -2,13 +2,20 @@ package org.eel.kitchen.jsonschema;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
 import org.eel.kitchen.jsonschema.util.jackson.JacksonUtils;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.UUID;
 
 public final class Utils
 {
+    private static final String RANDOM_UUID = UUID.randomUUID().toString();
+
     private static final ObjectWriter WRITER
         = JacksonUtils.getMapper().writerWithDefaultPrettyPrinter();
 
@@ -27,5 +34,21 @@ public final class Utils
         } finally {
             sw.close();
         }
+    }
+
+    public static String getHash(final String first, final String... other)
+    {
+        final HashFunction f = Hashing.sha1();
+
+        final Hasher hasher = f.newHasher();
+
+        hasher.putString(first);
+
+        for (final String s: other)
+            hasher.putString(s);
+
+        final HashCode hash = hasher.hash();
+
+        return hash.toString();
     }
 }
