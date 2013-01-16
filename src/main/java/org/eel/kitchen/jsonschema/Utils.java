66a8c156dfe47c4f2f48e403acb05901c4f460b0
Francis Galiegue <fgaliegue@gmail.com>
2013-01-16 23:39:46 +0100

    Remove some dead code

--- src/main/java/org/eel/kitchen/jsonschema/Utils.java
+++ src/main/java/org/eel/kitchen/jsonschema/Utils.java
@@ -3,21 +3,14 @@ package org.eel.kitchen.jsonschema;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.hash.HashCode;
-import com.google.common.hash.HashFunction;
-import com.google.common.hash.Hasher;
-import com.google.common.hash.Hashing;
 import org.eel.kitchen.jsonschema.util.jackson.JacksonUtils;
 
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Map;
-import java.util.UUID;
 
 public final class Utils
 {
-    private static final String RANDOM_UUID = UUID.randomUUID().toString();
-
     private static final ObjectWriter WRITER
         = JacksonUtils.getMapper().writerWithDefaultPrettyPrinter();
 
@@ -43,22 +36,6 @@ public final class Utils
         } finally {
             sw.close();
         }
-    }
-
-    public static String getHash(final String first, final String... other)
-    {
-        final HashFunction f = Hashing.sha1();
-
-        final Hasher hasher = f.newHasher().putString(RANDOM_UUID);
-
-        hasher.putString(first);
-
-        for (final String s: other)
-            hasher.putString(s);
-
-        final HashCode hash = hasher.hash();
-
-        return hash.toString();
     }
 
     /*
