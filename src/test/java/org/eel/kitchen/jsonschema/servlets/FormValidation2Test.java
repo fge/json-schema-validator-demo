4a561da853bc9393560545becb74ae7366aa415c
Francis Galiegue <fgaliegue@gmail.com>
2013-01-17 20:19:37 +0100

    Add more tests for second servlet

--- src/test/java/org/eel/kitchen/jsonschema/servlets/FormValidation2Test.java
+++ src/test/java/org/eel/kitchen/jsonschema/servlets/FormValidation2Test.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import static org.mockito.Mockito.*;
 
@@ -27,12 +28,16 @@ public final class FormValidation2Test
     private HttpServletRequest request;
     private HttpServletResponse response;
     private FormValidation2 servlet;
+    private PrintWriter writer;
 
     @BeforeMethod
     public void init()
+        throws IOException
     {
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
+        writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
         servlet = new FormValidation2();
     }
 
@@ -66,5 +71,51 @@ public final class FormValidation2Test
         verify(response).sendError(HttpServletResponse.SC_BAD_REQUEST,
             "Missing parameters");
         verify(request, never()).getRequestDispatcher(any(String.class));
+    }
+
+    @Test
+    public void necessaryDataIsReturned()
+        throws ServletException, IOException
+    {
+        final String schema = "{}";
+        // FIXME: see below
+        final String data = "{ }";
+
+        when(request.getParameter(ServletInputs.SCHEMA)).thenReturn(schema);
+        when(request.getParameter(ServletInputs.DATA)).thenReturn(data);
+
+        servlet.doPost(request, response);
+
+        verify(writer).write(eq(data));
+    }
+
+    @Test(dependsOnMethods = "necessaryDataIsReturned")
+    public void invalidSchemaRaisesAnError()
+        throws ServletException, IOException
+    {
+        final String schema = "foo";
+        final String data = "{}";
+
+        when(request.getParameter(ServletInputs.SCHEMA)).thenReturn(schema);
+        when(request.getParameter(ServletInputs.DATA)).thenReturn(data);
+
+        servlet.doPost(request, response);
+
+        verify(writer).write(argThat(IS_ERROR));
+    }
+
+    @Test(dependsOnMethods = "necessaryDataIsReturned")
+    public void invalidDataRaisesAnError()
+        throws ServletException, IOException
+    {
+        final String schema = "{}";
+        final String data = "foo";
+
+        when(request.getParameter(ServletInputs.SCHEMA)).thenReturn(schema);
+        when(request.getParameter(ServletInputs.DATA)).thenReturn(data);
+
+        servlet.doPost(request, response);
+
+        verify(writer).write(argThat(IS_ERROR));
     }
 }
