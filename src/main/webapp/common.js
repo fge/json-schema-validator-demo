fb647c943fb65368a06f440fb711348cd02e726c
Francis Galiegue <fgaliegue@gmail.com>
2013-02-24 23:12:53 +0100

    common.js: import some more common code

--- src/main/webapp/common.js
+++ src/main/webapp/common.js
@@ -37,4 +37,26 @@ var DomElements = {
     STARTHIDDEN: ".starthidden"
 };
 
+/*
+ * Function added to set the cursor position at a given offset in a text area
+ *
+ * Found at:
+ *
+ * http://stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area
+ */
+new function ($)
+{
+    $.fn.setCursorPosition = function (pos)
+    {
+        if ($(this).get(0).setSelectionRange) {
+            $(this).get(0).setSelectionRange(pos, pos);
+        } else if ($(this).get(0).createTextRange) {
+            var range = $(this).get(0).createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    }
+}(jQuery);
 
--- src/main/webapp/site.js
+++ src/main/webapp/site.js
@@ -84,29 +84,6 @@ function loadSamples()
     });
 }
 
-/*
- * Function added to set the cursor position at a given offset in a text area
- *
- * Found at:
- *
- * http://stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area
- */
-new function ($)
-{
-    $.fn.setCursorPosition = function (pos)
-    {
-        if ($(this).get(0).setSelectionRange) {
-            $(this).get(0).setSelectionRange(pos, pos);
-        } else if ($(this).get(0).createTextRange) {
-            var range = $(this).get(0).createTextRange();
-            range.collapse(true);
-            range.moveEnd('character', pos);
-            range.moveStart('character', pos);
-            range.select();
-        }
-    }
-}(jQuery);
-
 // Function to report a parse error
 function reportParseError(parseError, msgHandle, textArea)
 {
